5caf698f8b3ab2de6a64b632f89da4f6664a9209
Sergio Rios <sergioriosg@gmail.com>
2017-07-20 15:25:28 -0500

    creacion de datos sumarizados en memoria (bajo rendimiento)

--- public/js/activity.all.js
+++ public/js/activity.all.js
@@ -14,8 +14,27 @@
                     data.push(row);
                 }
             }
-            console.log(data);
-        // }
+            var byUser = {};
+            var byCountry = {};
+            var byTechnology = {};
+
+            data.forEach(function(element) {
+                byUser[element[0]] = byUser[element[0]] || {};
+                byUser[element[0]].time = byUser[element[0]].time || 0;
+                byUser[element[0]].time += parseFloat(element[12]);
+
+                byCountry[element[5]] = byCountry[element[5]] || {};
+                byCountry[element[5]].time = byCountry[element[5]].time || 0;
+                byCountry[element[5]].time += parseFloat(element[12]);
+
+                byTechnology[element[7]] = byTechnology[element[7]] || {};
+                byTechnology[element[7]].time = byTechnology[element[7]].time || 0;
+                byTechnology[element[7]].time += parseFloat(element[12]);
+            }, this);
+
+            console.log(byUser);
+            console.log(byCountry);
+            console.log(byTechnology);
     }
     
     var table = $('#table').DataTable({
@@ -42,16 +61,23 @@
 
             this.api().columns([0, 1, 4, 5, 6, 7, 8]).every(function(index) {
                 var column = this;
-
-                var select = $('<select class="table-filter"><option value="">Show All</option></select>')
+                var name = $(column.header()).text();
+                var select = $('<select class="table-filter" name="' + name + '"><option value="">Show All</option></select>')
                 // .appendTo($(column.footer()).empty())
                 .appendTo($(column.header()).append('<br />'))
                 .on('change', function(){
                     var val = $.fn.dataTable.util.escapeRegex($(this).val());
                     column.search(val ? '^'+val+'$':'', true, false).draw();
 
-                    console.log(table.data());
-                    processData(table.rows().data());
+                    // console.log(table.data());
+                    // processData(table.rows().data());
+                    var filtersData = {};
+                    $('.table-filter').each(function(el){
+                        if(!!$(this).val()){
+                            filtersData[$(this).attr('name')] = $(this).val();
+                        }
+                    });
+                    console.log($.param(filtersData));
                 });
                 column.data().unique().sort().each(function(d, j){
                     var val = $(d).text();
